9ed0933d88a44b1e753775782dc76d1d816ec810
Степан Сапогов <s.stepan777@mail.ru>
2023-11-27 17:57:49 +0500

    Наделал делов

---
+++ BookStore/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class User : IdentityUser
+    {
+        //дополнительные поля для каждого пользователя
+        //для преподавателя могут понадобиться данные о ФИО
+
+        [Required(ErrorMessage = "Введите фамилию")]   // сообщение об ошибке при валидации на стороне клиента
+        [Display(Name = "Фамилия")]
+        public string LastName { get; set; }   //отображение Фамилия вместо LastName
+
+        [Required(ErrorMessage = "Введите имя")]
+        [Display(Name = "Имя")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Отчество")]
+        public string? Patronymic { get; set; }
+    }
+}
