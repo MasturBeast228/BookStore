a32ccf177f4fe6f6cbb7815033d902d5cd8c4f15
Степан Сапогов <s.stepan777@mail.ru>
2023-11-29 16:05:55 +0500

    Исправлены ошибки регистрации контекста данных

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -1,4 +1,14 @@
+using BookStore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppCtx>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<AppCtx>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
